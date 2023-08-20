2e096e1dfc352d58cdf327c01692882185db9370
tharun-selvam <tharunselvam22@gmail.com>
2023-08-20 17:27:14 +0530

    Revert "added city2"
    This reverts commit 07ded55db151498b9fc0e736783a82b50d17f860.

--- hello.c
+++ hello.c
@@ -5,7 +5,6 @@ int main()
     printf("I am Tharun\n");
     printf("I am 18 years old\n");
     printf("I am Coimbatore\n");
-    printf("I am Chennai\n");
     printf("I am TN\n");
     //4th commit
     //5th commit
